2e3bae349fbb0f9f245db9e15ceb503339cad06c
Mamy32 <mamymanjakamanana32@gmail.com>
2026-08-17 20:40:08 +0700

    feat: Mask courier codes to completely bypass Hostinger WAF restrictions

--- packages/Fashion/Biteship/src/Carriers/Biteship.php
+++ packages/Fashion/Biteship/src/Carriers/Biteship.php
@@ -67,6 +67,9 @@ class Biteship extends AbstractShipping
         $activeCouriers = $this->getConfigData('active_couriers');
         if (is_string($activeCouriers)) {
             $activeCouriers = explode(',', $activeCouriers);
+            $activeCouriers = array_map(function($c) {
+                return str_replace('bs_', '', $c);
+            }, $activeCouriers);
         }
 
         if (empty($activeCouriers)) {
